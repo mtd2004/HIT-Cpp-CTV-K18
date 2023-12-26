4bf3073394dd3f39436429ccb1d7020863ec665d
mtd2004 <mtd2004.coder@gmail.com>
2023-12-26 23:46:00 +0700

    BTVN

--- Buoi4/Bai5.cpp
+++ Buoi4/Bai5.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std ;
+
 int main()
 {
 	int n ; cin >> n ;
 	int a[n] ;
 	for(int i = 0 ; i < n ; i++)
+	{
 		cin >> a[i] ;
+	}
 	int c100 = 0 , c200 = 0 ;
 	for(int i = 0 ; i < n ; i++)
 	{
@@ -14,6 +17,9 @@ int main()
 		else if(a[i] == 200)
 			c200++ ;
 	}
-	cout << c100 << " " << c200 << endl ;
+	if((c100 + c200 * 2) % 2 == 0)
+		cout << "YES" << endl ;
+	else
+		cout << "NO" << endl ;
 	return 0 ;
 }
